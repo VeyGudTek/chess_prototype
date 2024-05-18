a7f9fce135597db7e61067357aef2f2bbeef2911
Tim Lin <lintim03@gmail.com>
2024-05-17 20:18:13 -0500

    Finish create_board method

--- chess.py
+++ chess.py
@@ -21,6 +21,30 @@ class Game():
             new_board[1][i].piece = Pawn([1, i], 'black', new_board)
             new_board[6][i].piece = Pawn([6, i], 'white', new_board)
 
+        #Rooks
+        new_board[0][0].piece = Rook([0, 0], 'black', new_board)
+        new_board[0][7].piece = Rook([0, 7], 'black', new_board)
+        new_board[7][0].piece = Rook([7, 0], 'white', new_board)
+        new_board[7][7].piece = Rook([7, 7], 'white', new_board)
+
+        #Knights
+        new_board[0][1].piece = Knight([0, 1], 'black', new_board)
+        new_board[0][6].piece = Knight([0, 6], 'black', new_board)
+        new_board[7][1].piece = Knight([7, 1], 'white', new_board)
+        new_board[7][6].piece = Knight([7, 6], 'white', new_board)
+
+        #Bishops
+        new_board[0][2].piece = Bishop([0, 2], 'black', new_board)
+        new_board[0][5].piece = Bishop([0, 5], 'black', new_board)
+        new_board[7][2].piece = Bishop([7, 2], 'white', new_board)
+        new_board[7][5].piece = Bishop([7, 5], 'white', new_board)
+
+        #Kings and Queens
+        new_board[0][4].piece = King([0, 4], 'black', new_board)
+        new_board[0][3].piece = Queen([0, 3], 'black', new_board)
+        new_board[7][4].piece = King([7, 4], 'white', new_board)
+        new_board[7][3].piece = Queen([7, 3], 'white', new_board)
+
         return new_board
 
     def move_piece(self, old_coord, new_coord, color):
@@ -33,6 +57,7 @@ class Game():
             print("ogey")
             self.board[old_coord[0]][old_coord[1]].piece = None
             self.board[new_coord[0]][new_coord[1]].piece = selected_piece
+            #Move this to Piece Class, make sure to set Objects local coordinates
             return True
         else:
             print("Not a valid move.")
@@ -62,6 +87,7 @@ class Game():
 
 def test():
     game = Game()
+    '''
     game.board[0][0].piece = Pawn([0, 0], 'black', game.board)
     game.board[5][6].piece = Pawn([5, 6], 'black', game.board)
 
@@ -75,7 +101,7 @@ def test():
     game.board[1][3].piece = None
     print(game.board[3][4].piece.show_moves())
     game.move_piece([3, 5], [3, 5], 'black')
-
+    '''
     game.print_board()
     #print(game.convert_input(input()))
 
