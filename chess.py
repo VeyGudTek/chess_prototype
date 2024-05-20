e241ad0a64b246d8a8ca38384fa7585a8b8aa6de
Tim Lin <lintim03@gmail.com>
2024-05-20 16:18:22 -0500

    moved test function to own script

--- chess.py
+++ chess.py
@@ -100,25 +100,3 @@ class Game():
             return False, ''
 
         return (8 - int(input_1[1]), self.x_conversion[input_1[0]]), (8 - int(input_2[1]), self.x_conversion[input_2[0]])
-
-
-def test():
-    game = Game()
-
-    game.board[0][0].piece = Pawn((0, 0), 'black', game.board)
-    game.board[5][6].piece = Pawn((5, 6), 'black', game.board)
-
-    game.board[3][4].piece = Knight((3, 4), 'black', game.board)
-    game.board[4][3].piece = Bishop((4, 3), 'white', game.board)
-    game.board[5][4].piece = Rook((5, 4), 'white', game.board)
-    game.board[3][3].piece = King((3, 3), 'white', game.board)
-    game.board[3][6].piece = Queen((3, 6), 'white', game.board)
-    game.board[1][0].piece = None
-    game.board[1][3].piece = None
-    print(game.board[7][1].piece.show_moves())
-    print(game.convert_input('b1 a3'))
-    game.print_board()
-    #print(game.convert_input(input()))
-
-test()
-
